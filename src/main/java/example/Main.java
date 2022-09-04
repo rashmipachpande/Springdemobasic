b9a58d9b0148947aa104d964c13bb2998e5316f5
Rashmi Pachpande <69718479+rashmipachpande@users.noreply.github.com>
2022-09-04 10:16:08 +0530

    Add files via upload

---
+++ src/main/java/example/Main.java
@@ -0,0 +1,17 @@
+package example;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+
+	private static ApplicationContext context;
+	
+	public static void main(String[]args) {
+		context = new ClassPathXmlApplicationContext("beans.xml");
+
+		HelloBean helloBean = (HelloBean) context.getBean("HelloBean");
+	helloBean.sayHello();
+	}
+	
+}
